f23661b1f1482ba82615445fdf8f602f599ed9a3
Rutuja <98hrutuja@gmail.com>
2022-11-24 10:48:40 +0530

    My first code

---
+++ ABC-Project/index.js
@@ -0,0 +1,23 @@
+// const header = document.querySelector('.header-nav');
+
+// window.onscroll = function(){
+//     var top = window.scrollY;
+
+//     if (top >= 150){
+//         header.classList.add('active')
+//     }else{
+//         header.classList.remove('active');
+//     }
+
+// }
+
+function myfunc(){
+    let a = document.getElementById("scroll").value;
+    var top = window.scrollY;
+
+    if (top >= 150){
+        document.nav.style.background = "grey";
+    }
+
+
+}
